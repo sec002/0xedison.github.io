a705c588d9e6b0cbeefcbcce6b95f1469ff24c38
root <root@localhost.localdomain>
2016-09-28 14:03:40 +0800

    测试

--- 1.js
+++ 1.js
@@ -1,8 +1,6 @@
-// alert(9);
-// var sty = document.getElementsByTagName("fanPhotoAlbumDetail_list");
-// sty.style.display = "none";
-
-
+/*
+    this is a test  
+*/
 function test() {
     console.log(document.cookie);
     console.log(document.domain);
