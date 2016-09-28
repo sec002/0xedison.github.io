d561f9801680e7d11667e246f4b067da70a51941
root <root@localhost.localdomain>
2016-09-28 11:07:02 +0800

    测试

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
 // alert(9);
+
+
+var sty = document.getElementsByTagName("fanPhotoAlbumDetail_list");
+sty.style.display = "none";
+
+
 function test() {
     alert(document.cookie);
     alert(document.domain);
