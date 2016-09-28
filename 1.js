f60f2a5383c095df85197625e00147be4128f634
root <root@localhost.localdomain>
2016-09-28 11:17:04 +0800

    测试

--- 1.js
+++ 1.js
@@ -1,13 +1,11 @@
 // alert(9);
-
-
-var sty = document.getElementsByTagName("fanPhotoAlbumDetail_list");
+// var sty = document.getElementsByTagName("fanPhotoAlbumDetail_list");
 // sty.style.display = "none";
 
 
 function test() {
-    alert(document.cookie);
-    alert(document.domain);
+    console.log(document.cookie);
+    console.log(document.domain);
 }
 
 test();
