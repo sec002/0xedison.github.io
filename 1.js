cdb49d81afab59a8db92e27ea36012876cde4f80
root <root@localhost.localdomain>
2016-11-24 01:19:38 +0800

    test

--- 1.js
+++ 1.js
@@ -3,7 +3,7 @@
 */
 function test() {
     // alert("edison is my hero");
-    document.getElementsByTagName("body")[0].innerHTML='<h1 style="color:red"> edison is my hero</h1>';
+    document.getElementsByTagName("body")[0].innerHTML='<h1 style="backgroundColor:#000000;color:#00FF00;font-size:300%"> edison is my hero</h1>';
 }
 
 test();
