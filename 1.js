e6883223ddde8a2140e8a5cc1095a8f962935de0
root <root@localhost.localdomain>
2016-11-24 01:13:41 +0800

    test

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@
     this is a test  
 */
 function test() {
-    alert("edison is my hero");
+    //alert("edison is my hero");
     document.getElementsByTagName("body")[0].innerHTML='<h1 style="color:red"> edison is my hero</h1>';
 }
 
