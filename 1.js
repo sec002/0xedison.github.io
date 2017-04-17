505b6ec3d4d4937e1a5929eaf39bd6efff431397
0xedison <webshell@list.ru>
2017-04-17 17:59:59 +0800

    Update 1.js
    test

--- 1.js
+++ 1.js
@@ -1,9 +1,7 @@
 /*
     this is a test!
 */
-function test() {
-    alert("edison is my hero");
-    // document.getElementsByTagName("body")[0].innerHTML='<h1 style="background-color:#000000;color:#00FF00;font-size:300%"> edison is my hero</h1>';
-}
 
-test();
+window.onload = function () {
+    document.write('hello,world');
+}
